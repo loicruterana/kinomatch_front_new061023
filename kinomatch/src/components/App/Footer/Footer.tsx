f7ae17892632053bd3d5ee1871561d9f864d797a
Leroy-Alexandre <alex.gen068@gmail.com>
2023-05-10 11:04:08 +0200

    <feat>(<Footer.tsx, LegalModel.tsx>): modal done but footer not disappear

--- kinomatch/src/components/App/Footer/Footer.tsx
+++ kinomatch/src/components/App/Footer/Footer.tsx
@@ -7,21 +7,22 @@ import './styles.scss';
 function Footer() {
   const [showModal, setShowModal] = useState(false);
 
-  const openModal = () => {
-    setShowModal(showModal => !showModal);
+  const handleModal = () => {
+    setShowModal(!showModal);
   }
 
   return (
     <footer className='footer'>
-      <div className='footer-content'>
-        <p className='footer-content--copyright'>Copyright © 2023 KinoMatch</p>
-        <a className='footer-content--contact' href="#">Contact</a>
-        <button className='footer-content--legal' onClick={openModal}>Mentions légales</button>
+      <div className='footer__content'>
+        <p className='footer__content-copyright'>Copyright © 2023 KinoMatch</p>
+        <a className='footer__content-contact' href="mailto:fakeAdress@mail.fr">Contact</a>
+        <button className='footer__content-legal' onClick={handleModal}>Mentions légales</button>
+        {
+          showModal && <LegalModal showModal={showModal} setShowModal={setShowModal} />
+        }
       </div>
     </footer>
   )
 }
-
-
 
 export default Footer;
--- kinomatch/src/components/App/Footer/LegalModal/LegalModal.tsx
+++ kinomatch/src/components/App/Footer/LegalModal/LegalModal.tsx
@@ -1,16 +1,27 @@
 import './LegalModal.scss';
 
-// Modale des mentions légales
-function LegalModal() {
+
+interface LegalModalProps {
+  showModal: boolean;
+  setShowModal: boolean;
+}
+
+function LegalModal(props: LegalModalProps) {
+  const { showModal, setShowModal } = props;
+  
+  const handleModal = () => {
+    setShowModal(!showModal);
+  }
+
   return (
     <div className='container'>
       <div className='legalModal'>
-        <h2 className='legalModal-title'>Mentions légales</h2>
+        <h2 className='legalModal__title'>Mentions légales</h2>
         <p
-          className='legalModal-content'>`Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, eius, iusto tempore eveniet quasi impedit at exercitationem itaque animi voluptatem, perspiciatis hic magnam blanditiis beatae in repudiandae nesciunt consectetur. Possimus? Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, eius, iusto tempore eveniet quasi impedit at exercitationem itaque animi voluptatem, perspiciatis hic magnam blanditiis beatae in repudiandae nesciunt consectetur. Possimus?
+          className='legalModal__content'>`Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, eius, iusto tempore eveniet quasi impedit at exercitationem itaque animi voluptatem, perspiciatis hic magnam blanditiis beatae in repudiandae nesciunt consectetur. Possimus? Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, eius, iusto tempore eveniet quasi impedit at exercitationem itaque animi voluptatem, perspiciatis hic magnam blanditiis beatae in repudiandae nesciunt consectetur. Possimus?
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, eius, iusto tempore eveniet quasi impedit at exercitationem itaque animi voluptatem, perspiciatis hic magnam blanditiis beatae in repudiandae nesciunt consectetur. Possimus? Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, eius, iusto tempore eveniet quasi impedit at exercitationem itaque animi voluptatem, perspiciatis hic magnam blanditiis beatae in repudiandae nesciunt consectetur. Possimus?`
         </p>
-        <button className='legalModal-button'>Retour</button>
+        <button className='legalModal__button' onClick={handleModal}>Retour</button>
       </div>
     </div>
 
@@ -18,3 +29,6 @@ function LegalModal() {
 }
 
 export default LegalModal;
+
+
+
