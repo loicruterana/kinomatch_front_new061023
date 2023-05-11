73dd8518c3771dd702c69445e386f2fc25a3d5b0
olivierhuynh-web <olivier.huynh@yahoo.fr>
2023-05-11 11:10:43 +0200

    <build>(<main.tsx>): react-router-dom installation + create router in the app

---
+++ kinomatch/src/components/App/CreateProfile/CreateProfile.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const CreateProfile = () => {
+  return (
+    <div>CreateProfile</div>
+  )
+}
+
+export default CreateProfile;
--- kinomatch/src/main.tsx
+++ kinomatch/src/main.tsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/App/App.tsx'
 import './styles/index.scss'
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <BrowserRouter>
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </BrowserRouter>,
 )
