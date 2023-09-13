595db31ab0034b84bfc71f411f838d58d08c5237
Leroy-Alexandre <alex.gen068@gmail.com>
2023-09-13 14:16:41 +0200

    <fix>(<ReaquireAuth)>: Session refresh home corrected

--- kinomatch/src/components/App/Profile/RequireAuth/RequireAuth.tsx
+++ kinomatch/src/components/App/Profile/RequireAuth/RequireAuth.tsx
@@ -4,22 +4,20 @@ import NotConnected from '../../NotConnected/NotConnected';
 import { useUser } from '../../../../hooks/useUser';
 import Loading from '../../Loading/Loading';
 import { useNavigate } from 'react-router-dom';
-
 export const RequireAuth = ({ children }) => {
   const navigate: (path: string) => void = useNavigate();
   const { data, loading } = useUser();
-
-  // if (loading) {
-  //   return <Loading />;
-  // }
-
-  if (!data?.id) {
-    setTimeout(() => {
-      navigate(`/`);
-      console.log('on est déconnecté');
-    }, 1000);
-    return <NotConnected />;
-  }
-
+  setTimeout(() => {
+    if (loading) {
+      return <Loading />;
+    }
+    if (!data?.id) {
+      setTimeout(() => {
+        navigate(`/`);
+        console.log('on est déconnecté');
+      }, 1000);
+      return <NotConnected />;
+    }
+  }, 1000);
   return children;
 };
